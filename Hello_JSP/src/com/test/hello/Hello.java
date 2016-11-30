34aabcf116d70aa44489c4e8755ccbb41d6e20ef
Kim SM <ksm0322do@naver.com>
2016-11-30 11:50:22 +0900

    modify >> Hello.java : 012456 by Kim SM

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -4,6 +4,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("local Repository create Hello_jsp");
+		//012456 : hotfix edit by Kim
+		//hotfix edit by Kim SM
+		System.out.println("hotfix edit/modify");
+		//012456 : end
 	}
 
 }
