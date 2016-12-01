71cc4999240df15b828736c67516333184e64b61
Kimseonmin111 <ksm0322do@naver.com>
2016-12-01 10:09:11 +0900

    modify >> Hello.java : 0278944 pull by kim

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -20,6 +20,10 @@ public class Hello {
 		//0412518 : origin/master edit by kim
 		System.out.println("GitHub edit 4567");
 		//0412518 : end
+		
+		//0278944 : pull by kim
+		System.out.println("GitHub edit 4567891");
+		//0278944 :  end
 
 	}
 
